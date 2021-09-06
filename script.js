294b7a067e5c4558d2ab95c65530c5bff51860da
Kapadia Ehtesham <ehtesham.webosmotic@gmail.com>
2021-09-06 14:42:58 +0530

    all

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName("tab_content");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = document.getElementsByClassName("tab_links");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
